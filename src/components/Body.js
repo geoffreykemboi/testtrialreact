fee18bce9fee077e63b45e65b30e0d68d8263a0f
geoffreykemboi <geoffreyk126@gmail.com>
2025-04-20 22:55:10 +0300

    trialtest

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Body = () => {
+   return(
+       <main>
+       <ul>
+           <li>Welcome home!</li>
+           <li>When are we meeting?</li>
+           <li>Trump will visit Kenya</li>
+           <li>Meta is closed school!</li>
+       </ul>
+           </main>    
+   );
+};
+
+export default Body;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+        <a href="https://en.wikipedia.org/wiki/Season%27s_Greetings_(disambiguation)">Seasons Greetings</a>
+    </footer>
+       
+  );
+};
+
+export default Footer;
